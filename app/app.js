e7633bb2834721826505b73c1e28db3d68797e38
arielcessario <arielcessario@gmail.com>
2016-06-08 09:55:34 -0300

    Ariel

--- app/app.js
+++ app/app.js
@@ -14,6 +14,7 @@
         'acAutocomplete',
         'acUsuarios',
         'acUsuariosAdministracion',
+        'acUsuariosNuevo',
         'acProductos',
         'acProductosAdministracion',
         'acStocks',
