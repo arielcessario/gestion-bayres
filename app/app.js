8fc867acd4fd94d6c64ee5a824edd26677186a5a
arielcessario <arielcessario@gmail.com>
2016-06-23 20:48:21 -0300

    Ariel

--- app/app.js
+++ app/app.js
@@ -31,7 +31,9 @@
         'acResumenCajaDiaria',
         'acAbrirCerrarCaja',
         'LangTables',
-        'acUploads'
+        'acUploads',
+        'acAvisos',
+        'acAvisosAdministracion'
     ]).config(['$routeProvider', 'authProvider', function ($routeProvider, authProvider) {
             authProvider.init({
                 domain: 'ac-desarrollos.auth0.com',
@@ -83,6 +85,19 @@
                     loadMyCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
                         // you can lazy load files for an existing module
                         return $ocLazyLoad.load('usuarios/usuarios.js');
+                    }]
+                }
+            });
+
+
+            $routeProvider.when('/administracion/avisos', {
+                templateUrl: 'avisos/avisos.html',
+                controller: 'AvisosController',
+                data: {requiresLogin: true},
+                resolve: { // Any property in resolve should return a promise and is executed before the view is loaded
+                    loadMyCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
+                        // you can lazy load files for an existing module
+                        return $ocLazyLoad.load('avisos/avisos.js');
                     }]
                 }
             });
@@ -210,8 +225,8 @@
         }])
         .controller('AppCtrl', AppCtrl);
 
-    AppCtrl.$inject = ['UserService', '$location', '$rootScope', '$interval', 'ProductService', 'StockService', 'SucursalesService', '$window', '$scope'];
-    function AppCtrl(UserService, $location, $rootScope, $interval, ProductService, StockService, SucursalesService, $window, $scope) {
+    AppCtrl.$inject = ['UserService', '$location', '$rootScope', '$interval', 'ProductService', 'CajasService', 'SucursalesService', '$window', '$scope', 'AvisosService'];
+    function AppCtrl(UserService, $location, $rootScope, $interval, ProductService, CajasService, SucursalesService, $window, $scope, AvisosService) {
         var vm = this;
         vm.isLogged = false;
         vm.user = undefined;
@@ -219,17 +234,21 @@
         vm.caja = '';
         vm.time = new Date().format('dddd, mmmm d, yyyy h:MM TT');
         vm.isMoved = false;
+        vm.avisos_index = [];
+        vm.nuevos_avisos = false;
 
         var location = $location.path().split('/');
         vm.menu = location[1];
         vm.sub_menu = location[2];
+
+        vm.updateValorCaja = updateValorCaja;
 
         ProductService.get();
         //StockService.get();
 
         angular.element($window).bind("scroll", function (e) {
             vm.isMoved = this.pageYOffset > 88;
-            if(!$scope.$$pahse){
+            if (!$scope.$$pahse) {
                 $scope.$apply();
             }
         });
@@ -269,6 +288,10 @@
                     }
                 }
             });
+
+
+            $interval(getAvisos, 6000);
+            getAvisos();
             //SucursalesService.getByParams('sucursal_id', 'nombre', 'true')
         });
 
@@ -282,6 +305,64 @@
             vm.menu = location[1];
             vm.sub_menu = location[2];
         });
+
+        if(vm.user.data.sucursal_id =! undefined ){
+            $interval(getAvisos, 6000);
+            getAvisos();
+        }
+
+        function getAvisos() {
+            console.log('entra');
+            AvisosService.get().then(function (data) {
+                if (vm.avisos_index.length > 0 && vm.nuevos_avisos == false) {
+                    for (var i = 0; i < data.length; i++) {
+                        if (vm.avisos_index[i].aviso !== data[i].aviso) {
+                            console.log('nuevo');
+                            vm.nuevos_avisos = true;
+                        }
+                    }
+                }
+
+                vm.avisos_index = data;
+            });
+        }
+
+        function updateValorCaja() {
+            vm.saldoInicial = 0;
+            vm.saldoFinal = 0;
+            CajasService.getSaldoInicial(UserService.getFromToken().data.sucursal_id, UserService.getFromToken().data.caja_id, function (data) {
+
+                vm.saldoInicial = parseFloat(data.replace('"', ''));
+                vm.saldoFinal = vm.saldoInicial;
+                CajasService.getCajaDiaria(UserService.getFromToken().data.sucursal_id, UserService.getFromToken().data.caja_id, function (data) {
+                    var asientos = [];
+                    var detalles = [];
+                    var asiento = {};
+
+                    for (var i = 0; i < data.length; i++) {
+                        for (var x = 0; x < data[i].movimientos.length; x++) {
+
+                            //agrego el movimiento de caja - Estos son los totales que aparecen al final del movimiento
+
+                            if (data[i].movimientos[x].cuenta_id.indexOf('1.1.1.0') > -1 || // Caja chica
+                                data[i].movimientos[x].cuenta_id.indexOf('1.1.2.0') > -1 || // Deudores
+                                data[i].movimientos[x].cuenta_id.indexOf('1.1.4.0') > -1 || // Tarjeta
+                                data[i].movimientos[x].cuenta_id.indexOf('2.1.1.0') > -1 || // Proveedores a deuda
+                                data[i].movimientos[x].cuenta_id.indexOf('1.1.1.2') > -1    // CC CA MP ML
+                            ) {
+
+
+                                if (data[i].movimientos[x].cuenta_id.indexOf('1.1.1.0') > -1) {
+                                    vm.saldoFinal += parseFloat(data[i].movimientos[x].importe);
+                                }
+                            }
+
+                        }
+                    }
+
+                });
+            });
+        }
 
     }
 
---
+++ app/avisos/avisos.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+    angular.module('gestionBayres.avisos', [])
+        .controller('AvisosController', AvisosController);
+
+
+    AvisosController.$inject = [];
+    function AvisosController() {
+
+        var vm = this;
+
+    }
+})();
