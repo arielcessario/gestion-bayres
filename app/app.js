0866dfbfbb8dd11e9ac0275a54dc5399d6a4c067
arielcessario <arielcessario@gmail.com>
2016-06-24 08:38:51 -0300

    Ariel

--- app/app.js
+++ app/app.js
@@ -312,13 +312,10 @@
         }
 
         function getAvisos() {
-            console.log('entra');
             AvisosService.get().then(function (data) {
-                console.log(data);
                 if (vm.avisos_index.length > 0 && vm.nuevos_avisos == false) {
                     for (var i = 0; i < data.length; i++) {
                         if (vm.avisos_index[i].aviso !== data[i].aviso) {
-                            console.log('nuevo');
                             vm.nuevos_avisos = true;
                         }
                     }
